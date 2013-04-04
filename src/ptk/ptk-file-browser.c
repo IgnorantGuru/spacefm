5ca1de49fd0dcd25aa5aed68ff7c08a2a6a04cdc
IgnorantGuru <ignorantguru@gmx.com>
2013-04-04 14:52:53 -0600

    revert mouse buttons 6 7 as back/forward (for horz scrolling) #294

--- src/ptk/ptk-file-browser.c
+++ src/ptk/ptk-file-browser.c
@@ -3679,10 +3679,11 @@ on_folder_view_button_press_event ( GtkWidget *widget,
             file_browser->skip_release = TRUE;
             return TRUE;
         }
-        
-        if ( event->button > 3 && event->button < 10 )     //sfm
+
+        if ( event->button == 4 || event->button == 5 ||
+             event->button == 8 || event->button == 9 )   //sfm
         {
-            if ( event->button == 4 || event->button == 6 || event->button == 8 )
+            if ( event->button == 4 || event->button == 8 )
                 ptk_file_browser_go_back( NULL, file_browser );
             else
                 ptk_file_browser_go_forward( NULL, file_browser );
