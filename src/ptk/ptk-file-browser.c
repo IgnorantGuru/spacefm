1aa285d11fbad6d3bf77edcba35e02160d1dc56e
IgnorantGuru <ignorantguru@gmx.com>
2013-11-04 14:47:29 -0700

    [panel] column no autosize

--- src/ptk/ptk-file-browser.c
+++ src/ptk/ptk-file-browser.c
@@ -4535,12 +4535,11 @@ void init_list_view( PtkFileBrowser* file_browser, GtkTreeView* list_view )
         {
             // column width
             gtk_tree_view_column_set_min_width( col, 50 );
+            gtk_tree_view_column_set_sizing( col, GTK_TREE_VIEW_COLUMN_FIXED );
             set = xset_get_panel_mode( p, column_names[j], mode );
-            width = set->y ? atoi( set->y ) : 0;
+            width = set->y ? atoi( set->y ) : 100;
             if ( width )
             {
-                // only if width so first run is not fixed
-                gtk_tree_view_column_set_sizing( col, GTK_TREE_VIEW_COLUMN_FIXED );
                 if ( cols[j] == COL_FILE_NAME && !app_settings.always_show_tabs
                                 && file_browser->view_mode == PTK_FB_LIST_VIEW
                                 && gtk_notebook_get_n_pages(
