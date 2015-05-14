IgnorantGuru/spacefm

commit 525d3b488b39588f9c48210be5c9c104f1b5815c

add Run As Task option to file handlers; update spacefm.pot; pull all lang

src/settings.c
@@ -39,7 +39,7 @@
 #include "vfs-utils.h" /* for vfs_load_icon */
 #include "ptk-location-view.h"
 
-#define CONFIG_VERSION "32"
+#define CONFIG_VERSION "33"
 
 /* Dirty hack: check whether we are under LXDE or not */
 #define is_under_LXDE()     (g_getenv( "_LXSESSION_PID" ) != NULL)
@@ -1520,6 +1520,13 @@ void load_settings( char* config_dir )
             // remove old set from session file
             xset_free( old_set );
         }
+    }
+    if ( ver < 33 )  // < 1.0.2
+    {
+        // Default Mount ISO file handler has new Run As Task option enabled
+        set = xset_is( "hand_f_+iso" );
+        if ( !set->disable )  // user changed default handler
+            set->keep_terminal = XSET_B_TRUE;
     }
 
     // add default bookmarks

data/spacefm-mansrc-en.html
@@ -2938,7 +2938,7 @@ This text box contains a space-separated list of MIME types (such as text/plain)
 This text box contains a space-separated list of pathname globs to be used in addition to MIME type to identify files compatible with this handler.  For example, a Pathname pattern of "*.avi" will cause all AVI video files to be associated with this handler.  Note that the full pathname of the file (eg /home/user/videos/movie.avi) is tested against the pattern, so for advanced uses the pattern may contain directory parts (eg /home/user/videos/*.avi) to control how files in specific locations are handled.
 
 <p><b><u>Open Command</u></b><br>
-This text box contains the commands which will be run if this handler is selected to open one or more files.  The command(s) are run as a bash script, and may contain any valid bash syntax (version 4, or any greater version of bash you have installed).  If the Run In Terminal box is checked, the script will be run in your configured terminal emulator window (which may allow the user to enter information).  You can also edit the commands in your configured editor by clicking on 'Edit' in the upper-right corner, or press Alt-Enter while the cursor is in the box (remember to save the script in your editor when finished).
+This text box contains the commands which will be run if this handler is selected to open one or more files.  The command(s) are run as a bash script, and may contain any valid bash syntax (version 4, or any greater version of bash you have installed).  If the <b>Run As Task</b> option is checked, the script will be run synchronously (will appear in the Task Manager), and any error will be shown in a popup.  If the <b>Run In Terminal</b> option is checked, the script will be run in your configured terminal emulator window (which allows the user to enter information).  You can also edit the commands in your configured editor by clicking on <b>Edit</b> in the upper-right corner, or press Alt-Enter while the cursor is in the box (remember to save the script in your editor when finished).
 
 <p>Several variables will be replaced before your commands are run:
 <center><table width="70%">

commit 7a862b0a20a8c5dfb9a8df265a083a8ddb172bab

fix segfault on 1.0.1 upgrade introduced in 525d3b48

src/settings.c
@@ -1525,7 +1525,7 @@ void load_settings( char* config_dir )
     {
         // Default Mount ISO file handler has new Run As Task option enabled
         set = xset_is( "hand_f_+iso" );
-        if ( !set->disable )  // user changed default handler
+        if ( set && !set->disable )  // user changed default handler
             set->keep_terminal = XSET_B_TRUE;
     }
 