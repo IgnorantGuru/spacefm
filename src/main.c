IgnorantGuru/spacefm

commit f167b2831bc2556fc383cf30ae6ce4d117b67c04

fix issue #468

src/main.c
@@ -1349,7 +1349,9 @@ int main ( int argc, char *argv[] )
                 vfs_file_monitor_clean();
                 return ret == -1 ? 0 : ret;
             }
+            GDK_THREADS_ENTER();
             gtk_main();
+            GDK_THREADS_LEAVE();
             vfs_file_monitor_clean();
             return 0;
         }
@@ -1509,8 +1511,10 @@ int main ( int argc, char *argv[] )
     run = handle_parsed_commandline_args();
     app_settings.load_saved_tabs = TRUE;
 
+    GDK_THREADS_ENTER();
     if( run )   /* run the main loop */
         gtk_main();
+    GDK_THREADS_LEAVE();
 
     main_window_event( NULL, NULL, "evt_exit", 0, 0, NULL, 0, 0, 0, FALSE );
 

commit 181ff16916d7a6445db44da23107107c9358e195

fix hang on access error on startup #539 introduced in f167b28

src/main.c
@@ -1515,16 +1515,17 @@ int main ( int argc, char *argv[] )
 
     main_window_event( NULL, NULL, "evt_start", 0, 0, NULL, 0, 0, 0, FALSE );
 
+    /* handle_parsed_commandline_args needs to be within GDK_THREADS_ENTER or
+     * ptk_show_error() in ptk_file_browser_chdir() access err causes hang on
+     * GDK_THREADS_ENTER before gtk_main() */
+    GDK_THREADS_ENTER();
     /* handle the parsed result of command line args */
     run = handle_parsed_commandline_args();
     app_settings.load_saved_tabs = TRUE;
 
     if ( run )   /* run the main loop */
-    {
-        GDK_THREADS_ENTER();
         gtk_main();
-        GDK_THREADS_LEAVE();
-    }
+    GDK_THREADS_LEAVE();
 
     main_window_event( NULL, NULL, "evt_exit", 0, 0, NULL, 0, 0, 0, FALSE );
 